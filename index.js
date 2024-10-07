3c489b100a6d150aaab928ee825a418e73578ffd
pmg <gachigipm@gmail.com>
2024-10-07 12:40:52 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+  function superbowlWin(records){
+    for(let record of records){
+        if (record.result==="W"){
+            return record.year
+        }    }
+  }
+  [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ].find(superbowlWin);
+
